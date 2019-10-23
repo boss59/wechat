a1d26f54a770007d19c03d97305d50cad79d2f35
boss59 <2792593650@qq.com>
2019-10-23 23:36:09 +0800

    aa

--- app/Http/Controllers/wechat/CourseController.php
+++ app/Http/Controllers/wechat/CourseController.php
@@ -5,49 +5,61 @@ namespace App\Http\Controllers\wechat;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Weui\Course;
+
 class CourseController extends Controller
 {
     // 课程
-    public function course()
+    public function course(Request $request)
     {
-        // 网页授权
-        $app = urlencode(env('APP_URl').'/wechat/add_course');
-        // 第一步：用户同意授权，获取code
-        $url = "location:https://open.weixin.qq.com/connect/oauth2/authorize?appid=".env('WECGAT_APPID')."&redirect_uri=".$app."&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
-        // dd($url);
-        header($url);
-
+        $openid = $request->session()->get('openid');
+        if (empty($openid)) {
+            // 网页授权
+            $app = urlencode(env('APP_URL').'/wechat/add_course');
+            // 第一步：用户同意授权，获取code
+            $url = "location:https://open.weixin.qq.com/connect/oauth2/authorize?appid=".env('WECGAT_APPID') . "&redirect_uri=" . $app . "&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
+            header($url);
+        } else {
+            return redirect('/wechat/index_course');
+        }
     }
-    public function add_course()
+    public function add_course(Request $request)
     {
         $code = request()->input('code'); // 得到code
         $token = file_get_contents("https://api.weixin.qq.com/sns/oauth2/access_token?appid=".env('WECGAT_APPID')."&secret=".env('WECHET_SECRET')."&code=".$code."&grant_type=authorization_code");
         $userinfo = json_decode($token,1);
-        //dd($userinfo);
-        $openid=$userinfo['openid'];
-        // 第四步：拉取用户信息(需scope为 snsapi_userinfo)
-        $info = file_get_contents("https://api.weixin.qq.com/sns/userinfo?access_token=".$userinfo['access_token']."&openid=".$userinfo['openid']."&lang=zh_CN");
-        $userinfo = json_decode($info,1);
-        if($userinfo){
-            $res=Course::insert([
+//        dd($userinfo['openid']);
+        // 查库
+        $info=Course::where('openid',$userinfo['openid'])->first();
+        if (!$info){
+            // 第四步：拉取用户信息(需scope为 snsapi_userinfo)
+            $info = file_get_contents("https://api.weixin.qq.com/sns/userinfo?access_token=".$userinfo['access_token']."&openid=".$userinfo['openid']."&lang=zh_CN");
+            $userinfo = json_decode($info,1);
+            $data =[
+                'name'=>$userinfo['nickname'],
                 'openid'=>$userinfo['openid'],
-                'name' =>$userinfo['nickname'],
-                'one' =>0,
-                'two' =>0,
-                'three' =>0,
-                'four' =>0,
-                'num' =>0,
-            ]);
+            ];
+            $user_id=Course::insertGetId($data);//添加到用户表 获取userid
+            $res=Course::where('cid','=',$user_id)->first();
             if ($res){
-                return view('wechat.course',['openid'=>$userinfo['openid']]);
+                $request->session()->put('openid',$res);
+                return redirect('/wechat/index_course');
             }else{
-                return redirect('/wechat/course');
+                return redirect()->back();
             }
+        }else{
+            $request->session()->put('openid',$info);
+            return redirect('/wechat/index_course');
         }
     }
     // 课程
-    public function index_course(Request $resquest)
+    public function index_course(Request $request)
     {
+        $openid=$request->session()->get('openid')->toArray();
+//        if ()
+//        $res=Course::where(['openid'=>$openid['openid']])->update([
+//            'one'=>$userinfo['nickname'],
+//            'openid'=>$userinfo['openid'],
+//        ]);
         return view('wechat.course');
     }
 }
